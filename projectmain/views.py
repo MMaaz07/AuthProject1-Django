1f292787046a9302b01ce97c10425f99437c73fa
MMaaz07 <maazmohammed077@gmail.com>
2025-11-16 14:10:56 +0530

    Initial commit

---
+++ projectmain/views.py
@@ -0,0 +1,50 @@
+from django.http import HttpResponse
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login,authenticate,logout
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+
+def login_view(request):
+    if request.method == 'POST':
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+
+        user=authenticate(request,username=username,password=password)
+
+        if user:
+            login(request, user)
+            return redirect('home')
+        else:
+            return render(request,'login.html',{'error':'Invalid Credentials'})
+    return render(request, 'login.html')
+
+@login_required(login_url='login')
+def home(request):
+    return render(request, 'home.html')
+
+def register_user(request):
+    errors={}
+    if request.method == 'POST':
+        username=request.POST.get('username')
+        email=request.POST.get('email')
+        password=request.POST.get('password')
+        confirm_password=request.POST.get('confirm-password')
+
+
+        if confirm_password!=password:
+            errors['confirm_password']="Passwords do not match"
+
+        if User.objects.filter(username=username).exists():
+            errors['username'] = "Username already exists, click on Login"
+        
+        if not errors:
+            User.objects.create_user(username=username, email=email, password=password)
+            return redirect('login')
+    
+    return render(request, 'register.html',{'errors':errors})
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
